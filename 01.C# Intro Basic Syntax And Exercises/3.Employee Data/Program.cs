89964fded58c19a8db700af4b86629018343d603
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-07 15:15:29 +0300

    C# Intro Basic Syntax And Exercises

--- 01.C# Intro Basic Syntax And Exercises/3.Employee Data/Program.cs
+++ 01.C# Intro Basic Syntax And Exercises/3.Employee Data/Program.cs
@@ -11,8 +11,11 @@ namespace _3.Employee_Data
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
+
             int age = int.Parse(Console.ReadLine());
+
             int id = int.Parse(Console.ReadLine());
+
             double salary = double.Parse(Console.ReadLine());
 
             Console.WriteLine($"Name: {name}");
